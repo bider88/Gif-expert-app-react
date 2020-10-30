fcbe1e85f72b346bc5cb80918bb943eff20aa9a0
Irving Didier <bider88@gmail.com>
2020-10-30 16:25:43 -0600

    rename build by docs folder to be deployed into github pages

---
+++ docs/precache-manifest.bb55393c5109abce96caced403d49e4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60e4af71f301f5b62d4bff063e260e92",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd65c7af1410f4fae0ca",
+    "url": "/static/css/main.0b9ee8b7.chunk.css"
+  },
+  {
+    "revision": "0f6140dbcd9b418d88d9",
+    "url": "/static/js/2.e1db24a7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.e1db24a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd65c7af1410f4fae0ca",
+    "url": "/static/js/main.9f853ea5.chunk.js"
+  },
+  {
+    "revision": "2424585a9fc3f0980244",
+    "url": "/static/js/runtime-main.0218b45a.js"
+  }
+]);
